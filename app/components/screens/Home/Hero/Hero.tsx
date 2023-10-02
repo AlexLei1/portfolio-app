386885e8c97db2960fbb26295385cc2a6e0f37c2
Alex Lei <sasha.volkov2015@yandex.ru>
2023-10-02 22:57:33 +0300

    updated loader & added framer-motion

--- app/components/screens/Home/Hero/Hero.tsx
+++ app/components/screens/Home/Hero/Hero.tsx
@@ -2,28 +2,48 @@ import {FC, useState} from 'react'
 import styles from './Hero.module.scss'
 import { useCheckingScope } from '@/hooks/useCheckingScope';
 import TypingText from '@/ui/TypingText/TypingText';
+import {motion} from 'framer-motion'
 
 const Hero:FC = () => {
 	const {ref, inView, entry} = useCheckingScope()
-
+	const textAnimation = {
+		hidden: {
+			x: -100,
+			opacity: 0,
+		},
+		visible: (custom: number) => ({
+			x: 0,
+			opacity: 1,
+			transition: { delay: custom * 0.2}
+		}) 
+	}
 
 	return (
-		<section ref={ref} className={styles.section}>
+		<motion.section 	
+			initial='hidden'
+			whileInView='visible'
+			viewport={{amount: 0.2, once: true}}
+			ref={ref} 
+			className={styles.section}
+		>
 			<div className={styles.title}>
 				<TypingText inView={inView}>
 					Hi, my name is
 				</TypingText>
 			</div>
-			<div>
+
+			<motion.div custom={1} variants={textAnimation}>
 				<h2>Alex Lei.</h2>
-			</div>
-			<div>
+			</motion.div>
+
+			<motion.div custom={2} variants={textAnimation}>
 				<h3>I build things for the web.</h3>
-			</div>
+			</motion.div>
+
 			<div>
 				<p>Я инженер-программист, специализирующийся на создании цифровых приложений. Сейчас ищу работу в этой сфере</p>
 			</div>
-		</section>
+		</motion.section>
 	)
 }
 
--- app/components/ui/Loader/Loader.jsx
+++ app/components/ui/Loader/Loader.jsx
@@ -1,67 +1,18 @@
-import {FC, useState, useEffect} from 'react'
+import {useEffect} from 'react'
 import styles from './Loader.module.scss'
-import  Image  from 'next/image'
-import logoImage from '@/assets/images/logo.svg'
-import  anime  from 'animejs';
 import { ILoader } from '@/ui/Loader/type.loader';
 
 
 
 const Loader = ({finishLoading}) => {
-	const [isMounted, setIsMounted] = useState(false);
 
-	const animate = () => {
-		const loader = anime.timeline({
-			complete: () => finishLoading(),
-		});
-
-		loader
-		.add({
-			targets: '#logo path',
-			delay: 300,
-			duration: 1500,
-			easing: 'easeInOutQuart',
-			strokeDashoffset: [anime.setDashoffset, 0],
-		})
-		.add({
-			targets: '#logo #B',
-			duration: 700,
-			easing: 'easeInOutQuart',
-			opacity: 1,
-		})
-		.add({
-			targets: '#logo',
-			delay: 500,
-			duration: 300,
-			easing: 'easeInOutQuart',
-			opacity: 0,
-			scale: 0.1,
-		})
-		.add({
-			targets: '.loader',
-			duration: 200,
-			easing: 'easeInOutQuart',
-			opacity: 0,
-			zIndex: -1,
-		});
-	}
-	
 	useEffect(() => {
-    const timeout = setTimeout(() => setIsMounted(true), 10);
-    animate();
-    return () => clearTimeout(timeout);
-  }, []);
+		const timeout = setTimeout(() => finishLoading(), 3000);
+		return () => clearTimeout(timeout);
+  	}, []);
 
 	return (
-		<div className={styles.loader}>
-			<Image
-					src={logoImage}
-					width={300}
-					height={79.65}
-					alt="logo app"
-					draggable={false}
-				/>
-		</div>
+		<span className={styles.loader}></span>
 	)
 }
 
