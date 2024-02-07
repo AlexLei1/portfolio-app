6c870de0b28e0235e807a20bbb80db1bd4af10bd
Alex Lei <sasha.volkov2015@yandex.ru>
2024-02-07 20:17:31 +0300

    Updated photo file format from png to PNG

--- app/data/application.js
+++ app/data/application.js
@@ -4,7 +4,7 @@ export const applicationsData = [
 		title: 'Online Cinema',
 		description: 'Эта платформа предоставляет пользователям доступ к широкому спектру фильмов и мультфильмов. Это уникальное решение для тех, кто хочет наслаждаться любимыми фильмами и сериалами в любое время и из любой точки мира, используя различные устройства, такие как компьютеры, смартфоны и планшеты.',
 		stacks: ['SCSS', 'Next JS','Typescript','Axios','Redux'],
-		img: '/../public/photo/online-cinema.png',
+		img: '/../public/photo/online-cinema.PNG',
 		linkApp: 'https://github.com/AlexLei1/online-cinema-frontend',
 		linkGit: 'https://github.com/AlexLei1/online-cinema-frontend'
 	},
@@ -13,7 +13,7 @@ export const applicationsData = [
 		title: 'Pair Game',
 		description: 'На квадратном поле из закрытых карт игрок должен найти пары одинаковых карт, раскрывая их в последовательном порядке. Игрок начинает сначала с одной карты, затем со второй. Если обе карты раскрыты, они останутся открытыми до конца игры. В противном случае они перевернутся. Как только игрок открыл все пары на поле, игра считается завершенной.',
 		stacks: ['SCSS', 'React', 'Typescript'],
-		img: '/../public/photo/pair-game.png',
+		img: '/../public/photo/pair-game.PNG',
 		linkApp: 'https://github.com/AlexLei1/pair-game-app',
 		linkGit: 'https://github.com/AlexLei1/pair-game-app'
 	},	
@@ -22,7 +22,7 @@ export const applicationsData = [
 		title: 'Employee',
 		description: 'Это интуитивно понятное и простое в использовании приложение для управления сотрудниками, которое позволяет добавлять сотрудников и управлять их данными где угодно.',
 		stacks: ['SCSS', 'Next JS', 'Redux/Toolkit', 'Typescript'],
-		img: '/../public/photo/employee-login.png',
+		img: '/../public/photo/employee-login.PNG',
 		linkApp: 'https://github.com/AlexLei1/employees-app-frontend',
 		linkGit: 'https://github.com/AlexLei1/employees-app-frontend'
 	},
